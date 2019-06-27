403cbdd33c854c77c7bbc4e641c423c658b8f9b2
Eddthing CEO <sangou1270@gmail.com>
2019-06-28 00:35:06 +0900

    Initial Commit ver1.3

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const express = require('express');
+// const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function hendleListening () {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
-function handleHome(req, res) {
-    // console.log('Hi from Home!!!!!')
-    // console.log(req);
-    res.send('Hello from Home!!!!!!')
-}
-function handleProfile(req, res){
-    res.send("You are on my Profile")
-}
-app.get("/",handleHome);
+const hendleListening =() => 
+console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send('Hello from ass!!!!!!');
+const handleProfile=(req, res) => res.send("You are on my Profile");
+
+app.get("/",handleHome);  
 
 app.get("/profile", handleProfile);
 
