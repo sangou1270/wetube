314f8f1cadc6cd86578f13f21a5b1cf65d5074ac
Eddthing CEO <sangou1270@gmail.com>
2019-06-27 23:46:28 +0900

    Initial Commit ver1.2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hi");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function hendleListening () {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+function handleHome(req, res) {
+    // console.log('Hi from Home!!!!!')
+    // console.log(req);
+    res.send('Hello from Home!!!!!!')
+}
+function handleProfile(req, res){
+    res.send("You are on my Profile")
+}
+app.get("/",handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, hendleListening); //포트 번호
