111942bd85ae5d345c1970562ea3784baedc8e70
Eddthing CEO <sangou1270@gmail.com>
2019-07-11 00:46:12 +0900

    #2.24 Log in add User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,12 @@ export const postJoin = (req, res) => {
     }
 };
 
-export const login = (req, res) => res.render("login",{ pageTitle: "Login" });
+export const getLogin = (req, res) => 
+    res.render("login",{ pageTitle: "Login" });
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+}
+
 export const logout = (req, res) => res.render("logout",{ pageTitle: "Logout" });
 export const users = (req, res) => res.render("users",{ pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail",{ pageTitle: "User Detail" });
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,16 +1,19 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { getJoin, login, logout, postJoin } from "../controllers/userController";
+import { getJoin, logout, postJoin, getLogin, postLogin } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
+ 
