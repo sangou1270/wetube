8baf4c298c3f4ca1244144210c0ec300bdbe830f
Eddthing CEO <sangou1270@gmail.com>
2019-07-24 00:20:28 +0900

    #6.1 Local Authentication with Passport part One

--- init.js
+++ init.js
@@ -1,10 +1,11 @@
+import dotenv from "dotenv";
 import "./db";
 import app from "./app";
-import dotenv from "dotenv";
+
 dotenv.config();
 import "./models/Video";
 import "./models/Comment";
-
+import "./models/User";
 const PORT = process.env.PORT || 4000; // PORT는 .env에서 호출 함
 
 const handleListening = () =>
