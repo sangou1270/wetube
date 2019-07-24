d424147ac72ee573eb34c18724d8a7fa0972fe18
Eddthing CEO <sangou1270@gmail.com>
2019-07-24 21:11:50 +0900

    #6.6 Github Log In part One

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,12 @@ export const postLogin = passport.authenticate("local", {
   successRedirect: routes.home
 });
 
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
 export const logout = (req, res) => {
+  req.logout();
   // TODO: 사용자 로그아웃
   res.redirect(routes.home);
 };
